139613a67dc04a7a00f702665d3beba305a23b86
MOHAMMED IBRAHIM <ibrahimmuhammad018@gmail.com>
2022-10-22 12:31:33 +0000

    update welcome message  and out-putting Computer guess

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let username = prompt("Please enter username.");
 
 // Welcome user to the Game.
 
-console.log(`${username}, you are welcomed to Guessing Game!!!`);
+console.log(`Welcomed to YORDA Guessing Game, ${username}!!!`);
 
 // Variable declarations for loop
 
@@ -17,15 +17,15 @@ for (let i = 2; i < 11;) {
   console.log(`Welcome to stage ${level}`)
   let comPuter = Math.floor(Math.random() * i) + 1;
   let userPlayer = parseInt(prompt(`Guess a number between 1 and ${i}: `));
-  
- if (comPuter === userPlayer){
-   i++;
-   points++;
-   level++;
-   console.log(`Hurray, You now have ${points} points and have moved to stage ${level}`);
-     
- }else {
-   console.log(`Sorry, You misse. Please try again.`);
- }
+  console.log(`Computer guessed ${comPuter}`);
+  if (comPuter === userPlayer) {
+    i++;
+    points++;
+    level++;
+    console.log(`Hurray, You now have ${points} points and have moved to stage ${level}`);
+
+  } else {
+    console.log(`Sorry, You missed. Please try again.`);
+  }
 }
 
